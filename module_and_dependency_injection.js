796029ebfe5bd3b80db1892fa7f753e219fbc745
Zhiyuan Sun <sunzy0212@163.com>
2017-02-25 13:04:13 +0800

    Lazy instantiation of Dependencies

---
+++ module_and_dependency_injection.js
@@ -0,0 +1,107 @@
+function setupModuleLoader(window) {
+	var ensure = function(obj, name, factory) {
+		return obj[name] || (obj[name] = factory()); 
+	};
+
+	// extend angular to window
+	var angular = ensure(window, 'angular', Object);
+
+	// extend module to angular
+	ensure(angular, 'module', function() { 
+		var modules = {};
+		return function(name, requires) {
+			if (requires) {
+				return createModule(name, requires, modules);
+			} else {
+				return getModule(name, modules);
+			} };
+		}
+	);
+
+	
+	var createModule = function(name, requires, modules) { 
+		if (name === 'hasOwnProperty') {
+			throw 'hasOwnProperty is not a valid module name'; 
+		}
+		var invokeQueue = []; 
+		var invokeLater = function(method) { 
+			return function() {
+				invokeQueue.push([method, arguments]);
+				return moduleInstance; 
+			};
+		};
+
+		var moduleInstance = {
+			name: name,
+			requires: requires,
+			constant: invokeLater('constant'), 
+			provider: invokeLater('provider'),
+			_invokeQueue: invokeQueue
+		};
+		modules[name] = moduleInstance;
+		return moduleInstance; 
+	};
+}
+
+
+function createInjector(modulesToLoad) { 
+	var providerCache = {};	// cache of provider
+	var instanceCache = {};	// cache of instance of provider
+	var $provide = {
+		constant: function(key, value) {
+			instanceCache[key] = value;
+		},
+		provider: function(key, provider) { 
+			providerCache[key +  'Provider' ] = provider;
+		}
+	};
+
+	var loadedModules = {};
+	_.forEach(modulesToLoad, function loadModule(moduleName) { 
+		if (!loadedModules.hasOwnProperty(moduleName)) {
+			loadedModules[moduleName] = true;
+			var module = angular.module(moduleName); 
+			_.forEach(module.requires, loadModule); 
+			_.forEach(module._invokeQueue, function(invokeArgs) {
+				var method = invokeArgs[0];
+				var args = invokeArgs[1]; 
+				$provide[method].apply($provide, args);
+			});
+		}
+	});
+
+	function invoke(fn, self, locals) {
+		var args = _.map(fn.$inject, function(token) {
+			if (_.isString(token)) {
+				return locals && locals.hasOwnProperty(token) ?
+					locals[token] : getService[token]; 
+			} else {
+				throw 'Incorrect injection token! Expected a string, got '+token; 
+			}
+		});
+		return fn.apply(self, args); 
+	}
+
+	function getService(name) {
+		if (instanceCache.hasOwnProperty(name)) {
+			return instanceCache[name];
+		} else if (providerCache.hasOwnProperty(name + 'Provider' )) {
+			var provider = providerCache[name + 'Provider' ];
+			return invoke(provider.$get, provider); 
+		}	
+	}
+
+	return {
+		has: function(key) {
+			return instanceCache.hasOwnProperty(key)
+				|| providerCache.hasOwnProperty(key + 'Provider');
+		},
+		get: getService
+		invoke: invoke,
+		annotate: annotate
+	}; 
+}
+
+
+
+
